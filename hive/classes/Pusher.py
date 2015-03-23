686d77135f89195734a2133b93f16a12a009f0d4
Angus Hollands <goosey15@gmail.com>
2015-03-23 18:43:00 +0000

    PEP8

--- hive/classes/Pusher.py
+++ hive/classes/Pusher.py
@@ -1,12 +1,15 @@
 class Pusher(object):
-  def __init__(self, parent):
-    self._parent = parent
-    self._targets = []
-  def add_target(self, targetfunc, targetname = None):
-    assert callable(targetfunc)
-    assert targetname not in self._targets, (targetname, self._parent)
-    self._targets.append((targetname, targetfunc))
-  def push(self, *args, **kwargs):
-    for tname, tfunc in self._targets: 
-      #TODO: exception handling
-      tfunc(*args, **kwargs)
+
+    def __init__(self, parent):
+        self._parent = parent
+        self._targets = []
+
+    def add_target(self, targetfunc, targetname=None):
+        assert callable(targetfunc)
+        assert targetname not in self._targets, (targetname, self._parent)
+        self._targets.append((targetname, targetfunc))
+
+    def push(self, *args, **kwargs):
+        for tname, tfunc in self._targets: 
+            #TODO: exception handling
+            tfunc(*args, **kwargs)
