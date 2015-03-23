e368ad724f03df1b06ae778fd0599bbebf3dd302
Angus Hollands <goosey15@gmail.com>
2015-03-23 20:40:48 +0000

    PEP8

--- hive/mixins/Stateful.py
+++ hive/mixins/Stateful.py
@@ -5,11 +5,14 @@ Instead of adding the whole hive instance, a *Python attribute* is added to the 
 The getter/setter of this attribute are hiveinstance._hive_stateful.getter and hiveinstance._hive_stateful.setter, 
  respectively
  
-A Stateful object's getter/setter always accept a runhive object, which will be None in immediate mode 
+A Stateful object's getter/setter always accept a run_hive object, which will be None in immediate mode
 """
 
+
 class Stateful(object):
-    def _hive_stateful_getter(self, runhive):
+
+    def _hive_stateful_getter(self, run_hive):
         raise NotImplementedError
-    def _hive_stateful_setter(self, runhive, value):
+
+    def _hive_stateful_setter(self, run_hive, value):
         raise NotImplementedError    
