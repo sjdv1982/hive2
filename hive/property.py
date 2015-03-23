e8a9401fa55b302d2a6233ffaf4ad6d1092b62e1
Angus Hollands <goosey15@gmail.com>
2015-03-23 19:59:45 +0000

    PEP8 rename

--- hive/property.py
+++ hive/property.py
@@ -3,40 +3,40 @@ from .mixins import Stateful, Exportable, Bindable
 from . import get_mode, get_building_hive
 
 
-def tupletype(t):
-    if t is None:
+def _check_tuple_type(value):
+    if isinstance(value, str):
+        return
+
+    assert isinstance(value, tuple), value
+    for entry in value:
+        _check_tuple_type(entry)
+
+
+def tuple_type(value):
+    if value is None:
         return ()
 
-    if isinstance(t, str):
-        return (t,)
-
-    assert isinstance(t, tuple), t
+    if isinstance(value, str):
+        return (value,)
     
-    def _check_tupletype(t):
-        if isinstance(t, str):
-            return
-
-        assert isinstance(t, tuple), t
-        for tt in t:
-            _check_tupletype(tt)
-    
-    _check_tupletype(t)
-    return t
+    _check_tuple_type(value)
+    return value
 
 
 class Property(Stateful, Bindable, Exportable):
 
-    def __init__(self, cls, attr, datatype, startvalue): 
-        self._hivecls = get_building_hive()
+    def __init__(self, cls, attr, data_type, start_value):
+        self._hive_cls = get_building_hive()
         self._cls = cls
         self._attr = attr
-        self.datatype = datatype
-        self.startvalue = startvalue
 
-    def _hive_stateful_getter(self, runhive):
+        self.data_type = data_type
+        self.start_value = start_value
+
+    def _hive_stateful_getter(self, run_hive):
         cls = self._cls
-        assert id(cls) in runhive._hive_buildclass_instances, cls
-        instance = runhive._hive_buildclass_instances[id(cls)]        
+        assert id(cls) in run_hive._hive_buildclass_instances, cls
+        instance = run_hive._hive_buildclass_instances[id(cls)]
         return getattr(instance, self._attr)
 
     def _hive_stateful_setter(self, runhive, value):
@@ -52,17 +52,19 @@ class Property(Stateful, Bindable, Exportable):
         cls = self._cls
         assert id(cls) in runhive._hive_buildclass_instances, cls
         instance = runhive._hive_buildclass_instances[id(cls)]
-        if self.startvalue is not None or not hasattr(instance, self._attr):
-            setattr(instance, self._attr, self.startvalue)
+        if self.start_value is not None or not hasattr(instance, self._attr):
+            setattr(instance, self._attr, self.start_value)
 
         return self
 
 
-def property(cls, attr, datatype=None, startvalue=None): 
-    datatype = tupletype(datatype)
+def property(cls, attr, data_type=None, start_value=None):
+    data_type = tuple_type(data_type)
+
     if get_mode() == "immediate":
         raise ValueError("hive.property cannot be used in immediate mode")
 
     else:
-        assert isinstance(cls, HiveMethodWrapper) #property(cls) must be the cls argument in build(cls, i, ex, args)
-        return Property(cls._cls, attr, datatype, startvalue) 
+        assert isinstance(cls, HiveMethodWrapper), "hive.property(cls) must be the cls argument in" \
+                                                   " build(cls, i, ex, args)"
+        return Property(cls._cls, attr, data_type, start_value)
