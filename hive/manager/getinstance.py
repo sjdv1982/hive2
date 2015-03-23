f9bbecb39cb81ac3075f3dce8bb99baaa75a29c1
Angus Hollands <goosey15@gmail.com>
2015-03-23 18:43:06 +0000

    PEP8

--- hive/manager/getinstance.py
+++ hive/manager/getinstance.py
@@ -8,23 +8,25 @@ class partialmethod(partial):
     def __get__(self, instance, owner):
         if instance is None:
             return self
-        return partial(self.func, instance,
-                       *(self.args or ()), **(self.keywords or {}))
+        return partial(self.func, instance, *(self.args or ()), **(self.keywords or {}))
 ###
 
 _hiveobjects = WeakKeyDictionary()
 
+
 def register_hiveobject(hiveobject):
-  if hiveobject not in _hiveobjects:
-    _hiveobjects[hiveobject] = {}
+    if hiveobject not in _hiveobjects:
+        _hiveobjects[hiveobject] = {}
+
 
 def getinstance_manager(self, func, hiveobject):
-  assert hiveobject in _hiveobjects, hiveobject
-  if self not in _hiveobjects[hiveobject]:
-    _hiveobjects[hiveobject][self] = func(self, hiveobject)
-  return _hiveobjects[hiveobject][self]
-  
+    assert hiveobject in _hiveobjects, hiveobject
+    if self not in _hiveobjects[hiveobject]:
+        _hiveobjects[hiveobject][self] = func(self, hiveobject)
+    return _hiveobjects[hiveobject][self]
+
+
 def getinstance(getinstancefunc):
-  func = partialmethod(getinstance_manager, getinstancefunc)
-  functools.update_wrapper(func, getinstancefunc)
-  return func
+    func = partialmethod(getinstance_manager, getinstancefunc)
+    functools.update_wrapper(func, getinstancefunc)
+    return func
--- hive/manager/register_bee.py
+++ hive/manager/register_bee.py
@@ -1,12 +1,15 @@
 _bees = []
 
+
 def register_bee(bee):
-  assert len(_bees) > 0
-  _bees[-1].append(bee)
-  
+    assert len(_bees) > 0
+    _bees[-1].append(bee)
+
+
 def register_bee_pop():
-  assert len(_bees) > 0 #can't pop before push!
-  return _bees.pop()
-  
-def register_bee_push():  
-  _bees.append([])
+    assert len(_bees) > 0 #can't pop before push!
+    return _bees.pop()
+
+
+def register_bee_push():    
+    _bees.append([])
