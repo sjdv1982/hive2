e81d57f42e2887327613c6ce0aa9fc736509b038
Angus Hollands <goosey15@gmail.com>
2015-05-07 14:22:08 +0100

    Use functools.wraps

--- hive/manager/memoize.py
+++ hive/manager/memoize.py
@@ -1,4 +1,5 @@
 from weakref import WeakKeyDictionary
+from functools import wraps
 
 
 class Memoizer:
@@ -9,6 +10,7 @@ class Memoizer:
     def __call__(self, func):
         func_instance_cache = self._cache[func] = WeakKeyDictionary()
 
+        @wraps(func)
         def wrapper(self, *args):
             try:
                 results_cache = func_instance_cache[self]
