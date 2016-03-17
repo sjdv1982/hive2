559960efffa60d0556ba31c33275d2d6fc60344c
Angus Hollands <goosey15@gmail.com>
2016-03-17 13:10:40 +0000

    Make use of Instantiator factory

--- dragonfly/entity/instantiate.py
+++ dragonfly/entity/instantiate.py
@@ -1,20 +1,9 @@
 from .bind import bind_info as entity_bind_info
 
 from ..app import bind_info as app_bind_info
-from ..bind import Instantiator as _Instantiator, get_active_bind_environments, get_bind_bases
+from ..bind import create_instantiator
 from ..event import bind_info as event_bind_info
 
 
 bind_infos = (app_bind_info, event_bind_info, entity_bind_info)
-
-
-def build_entity_instantiate(i, ex, args, meta_args):
-    bind_environments = get_active_bind_environments(bind_infos, meta_args)
-
-    # Update bind environment to use new bases
-    environment_class = i.bind_meta_class.start_value
-    i.bind_meta_class.start_value = environment_class.extend("EntityBindEnvironment", bases=tuple(bind_environments))
-
-
-Instantiator = _Instantiator.extend("EntityInstantiator", builder=build_entity_instantiate,
-                                    bases=get_bind_bases(bind_infos))
+Instantiator = create_instantiator(bind_infos)
