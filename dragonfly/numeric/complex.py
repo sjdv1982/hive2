5231e6ad19bc8d7af4d40cac6c0b813e95e93753
Angus Hollands <goosey15@gmail.com>
2016-04-13 02:49:00 +0100

    Add conjugate op

--- dragonfly/numeric/complex.py
+++ dragonfly/numeric/complex.py
@@ -2,16 +2,16 @@ import hive
 
 
 def declare_complex(meta_args):
-    meta_args.mode = hive.parameter('str', 'compose', options={'compose', 'decompose'})
+    meta_args.mode = hive.parameter('str', 'compose', options={'compose', 'decompose', 'conjugate'})
 
 
 def build_complex(i, ex, args, meta_args):
     """Compose/decompose complex number"""
     i.value = hive.attribute('complex')
-    i.real = hive.attribute('float')
-    i.imag = hive.attribute('float')
 
     if meta_args.mode == 'compose':
+        i.real = hive.attribute('float')
+        i.imag = hive.attribute('float')
         i.pull_imag = hive.pull_in(i.imag)
         i.pull_real = hive.pull_in(i.real)
 
@@ -31,6 +31,8 @@ def build_complex(i, ex, args, meta_args):
         hive.trigger(i.pull_real, i.build_value)
 
     elif meta_args.mode == 'decompose':
+        i.real = hive.attribute('float')
+        i.imag = hive.attribute('float')
         i.pull_imag = hive.pull_out(i.imag)
         i.pull_real = hive.pull_out(i.real)
 
@@ -50,6 +52,22 @@ def build_complex(i, ex, args, meta_args):
         hive.trigger(i.pull_imag, i.pull_value, pretrigger=True)
         hive.trigger(i.pull_real, i.pull_value, pretrigger=True)
         hive.trigger(i.pull_value, i.build_value)
+
+    elif meta_args.mode == 'conjugate':
+        i.conjugate = hive.attribute('complex')
+        i.pull_conjugate = hive.pull_out(i.conjugate)
+        ex.conjugate = hive.output(i.pull_conjugate)
+
+        i.pull_value = hive.pull_in(i.value)
+        ex.value = hive.antenna(i.pull_value)
+
+        def build_conjugate(self):
+            self._conjugate = self._value.conjugate()
+
+        i.build_conjugate = hive.modifier(build_conjugate)
+
+        hive.trigger(i.pull_conjugate, i.pull_value, pretrigger=True)
+        hive.trigger(i.pull_value, i.build_conjugate)
 
 
 Complex = hive.dyna_hive("Complex", build_complex, declarator=declare_complex)
